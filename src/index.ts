20a59d6f7104b7ae17fcc1b950e1369abca5cebc
Lucas Calabria <lucascalabria86@gmail.com>
2023-01-26 13:54:58 -0300

    All features done

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { db } from './database/knex'
 
+import { TTask, TTaskWithUsers, TUser, TUserTask } from './types'
+
 const app = express()
 
 app.use(cors())
@@ -11,12 +13,12 @@ app.listen(3003, () => {
     console.log(`Servidor rodando na porta ${3003}`)
 })
 
-                        //USERS//
+//USERS//
 
 //GetAllUsers
 app.get("/users", async (req: Request, res: Response) => {
     try {
-		const result = await db("users")
+        const result = await db("users")
         res.status(200).send({ users: result })
     } catch (error) {
         console.log(error)
@@ -28,7 +30,7 @@ app.get("/users", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -37,10 +39,16 @@ app.get("/users", async (req: Request, res: Response) => {
 //SearchUserByName
 app.get("/users/:name", async (req: Request, res: Response) => {
     try {
-        const q = req.params.name
+        const q = req.params.name as string | undefined
 
-		const result = await db("users").where("name", "LIKE", `%${q}%`)
-        res.status(200).send({ user: result })
+        if (q === undefined) {
+            const result = await db("users")
+            res.status(200).send(result)
+        } else {
+            const result = await db("users").where("name", "LIKE", `%${q}%`)
+            res.status(200).send(result)
+        }
+
     } catch (error) {
         console.log(error)
 
@@ -51,7 +59,7 @@ app.get("/users/:name", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -64,14 +72,64 @@ app.post("/users", async (req: Request, res: Response) => {
         const email = req.body.email
         const password = req.body.password
 
-        const newUser = {
-            id, 
+        if (typeof id !== "string") {
+            res.status(400)
+            throw new Error("Invalid ID, must be a string")
+        }
+
+        if (id.length < 4) {
+            res.status(400)
+            throw new Error("Invalid ID, must have at least 4 characters")
+        }
+
+        if (id[0] !== "f") {
+            res.status(400)
+            throw new Error("Invalid ID, must begin with 'f'")
+        }
+
+        if (typeof name !== "string") {
+            res.status(400)
+            throw new Error("Invalid Name, must be a string")
+        }
+
+        if (name.length < 2) {
+            res.status(400)
+            throw new Error("Invalid Name, must have at least 2 characters")
+        }
+
+        if (typeof email !== "string") {
+            res.status(400)
+            throw new Error("Invalid Email, must be a string")
+        }
+
+        if (email.includes("@")) {
+        } else {
+            res.status(400)
+            throw new Error("Invalid Email, try again")
+        }
+
+        const [userId]: TUser[] | undefined[] = await db("users").where({ id })
+
+        if (userId) {
+            res.status(400)
+            throw new Error("Invalid ID, this user already exists")
+        }
+
+        const [userEmail]: TUser[] | undefined[] = await db("users").where({ email })
+
+        if (userEmail) {
+            res.status(400)
+            throw new Error("Invalid Email, this user already exists")
+        }
+
+        const newUser: TUser = {
+            id,
             name,
             email,
             password
         }
 
-		await db("users").insert(newUser)
+        await db("users").insert(newUser)
         res.status(200).send(`${name} created successfully`)
     } catch (error) {
         console.log(error)
@@ -83,7 +141,7 @@ app.post("/users", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -93,18 +151,23 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
     try {
         const idToDelete = req.params.id as string
 
-        if(idToDelete !== undefined) {
-            if(typeof idToDelete !== "string" ) {
+        if (idToDelete[0] !== "f") {
+            res.status(400)
+            throw new Error("ID must begin with 'f'")
+        }
+
+        if (idToDelete !== undefined) {
+            if (typeof idToDelete !== "string") {
                 res.status(400)
                 throw new Error("Invalid ID, must be a string")
             }
-            if(idToDelete.length < 1 ) {
+            if (idToDelete.length < 1) {
                 res.status(400)
                 throw new Error("Invalid ID, must have at least 2 characters")
             }
         }
 
-        const [user] = await db("users").where({ id: idToDelete })
+        const [user]: TUser[] | undefined[] = await db("users").where({ id: idToDelete })
         if (user) {
             await db("users_tasks").del().where({ user_id: idToDelete })
             await db("users").del().where({ id: idToDelete })
@@ -124,17 +187,17 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
 
-                        //TASKS
+//TASKS
 
 //GetAllTasks
 app.get("/tasks", async (req: Request, res: Response) => {
     try {
-		const result = await db("tasks")
+        const result = await db("tasks")
         res.status(200).send({ tasks: result })
     } catch (error) {
         console.log(error)
@@ -146,29 +209,7 @@ app.get("/tasks", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
-        }
-    }
-})
-
-//SearchTaskByTitle
-app.get("/tasks/:title", async (req: Request, res: Response) => {
-    try {
-        const q = req.params.title
-
-		const result = await db("tasks").where("title", "LIKE", `%${q}%`)
-        res.status(200).send({ task: result })
-    } catch (error) {
-        console.log(error)
-
-        if (req.statusCode === 200) {
-            res.status(500)
-        }
-
-        if (error instanceof Error) {
-            res.send(error.message)
-        } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -180,14 +221,52 @@ app.post("/tasks", async (req: Request, res: Response) => {
         const title = req.body.title
         const description = req.body.description
 
+        if (typeof id !== "string") {
+            res.status(400)
+            throw new Error("Invalid ID, must be a string")
+        }
+
+        if (id.length < 4) {
+            res.status(400)
+            throw new Error("Invalid ID, must have at least 4 characters")
+        }
+
+        if (typeof title !== "string") {
+            res.status(400)
+            throw new Error("Invalid Title, must be a string")
+        }
+
+        if (title.length < 2) {
+            res.status(400)
+            throw new Error("Invalid Title, must have at least 2 characters")
+        }
+
+        if (typeof description !== "string") {
+            res.status(400)
+            throw new Error("Invalid Description, must be a string")
+        }
+
+        if (description.length < 4) {
+            res.status(400)
+            throw new Error("Invalid Title, must have at least 4 characters")
+        }
+
+        const [task]: TTask[] | undefined[] = await db("tasks").where({ id })
+
+        if (task) {
+            res.status(400)
+            throw new Error("This ID already exists")
+        }
+
         const newTask = {
-            id, 
+            id,
             title,
             description,
         }
 
-		await db("tasks").insert(newTask)
+        await db("tasks").insert(newTask)
         res.status(200).send(`${title} created successfully`)
+
     } catch (error) {
         console.log(error)
 
@@ -198,7 +277,7 @@ app.post("/tasks", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -214,47 +293,51 @@ app.put("/tasks/:id", async (req: Request, res: Response) => {
         const newCreatedAt = req.body.createdAt
         const newStatus = req.body.status
 
-        if(newId !== undefined) {
-            if(typeof newId !== "string" ) {
+        if (newId !== undefined) {
+            if (typeof newId !== "string") {
                 res.status(400)
                 throw new Error("Invalid ID, must be a string")
             }
-            if(newId.length < 1 ) {
+            if (newId.length < 4) {
                 res.status(400)
-                throw new Error("Invalid ID, must have at least 2 characters")
+                throw new Error("Invalid ID, must have at least 4 characters")
+            }
+            if(newId[0] !== "t"){
+            res.status(400)
+            throw new Error("ID must begin with 't'")
             }
         }
-        if(newTitle !== undefined) {
-            if(typeof newTitle !== "string" ) {
+        if (newTitle !== undefined) {
+            if (typeof newTitle !== "string") {
                 res.status(400)
                 throw new Error("Invalid Title, must be a string")
             }
-            if(newTitle.length < 1 ) {
+            if (newTitle.length < 2) {
                 res.status(400)
                 throw new Error("Invalid Title, must have at least 2 characters")
             }
         }
-        if(newDescription !== undefined) {
-            if(typeof newDescription !== "string" ) {
+        if (newDescription !== undefined) {
+            if (typeof newDescription !== "string") {
                 res.status(400)
                 throw new Error("Invalid Description, must be a string")
             }
-            if(newDescription.length < 1 ) {
+            if (newDescription.length < 1) {
                 res.status(400)
                 throw new Error("Invalid Description, must have at least 2 characters")
             }
         }
 
-        if(newStatus !== undefined) {
-            if(typeof newStatus !== "number" ) {
+        if (newStatus !== undefined) {
+            if (typeof newStatus !== "number") {
                 res.status(400)
-                throw new Error("Invalid Status, must be a number")
+                throw new Error("Invalid Status, must be a number (0 or 1)")
             }
         }
 
-        const [task] = await db("tasks").where({ id: idToEdit })
+        const [task]: TTask[] | undefined[] = await db("tasks").where({ id: idToEdit })
 
-        if(!task) {
+        if (!task) {
             throw new Error("ID not found, please try again.")
         }
 
@@ -263,11 +346,12 @@ app.put("/tasks/:id", async (req: Request, res: Response) => {
             title: newTitle || task.title,
             description: newDescription || task.description,
             created_at: newCreatedAt || task.created_at,
-            status: newStatus || task.status
+            status: isNaN(newStatus) ? task.status : newStatus
         }
 
-		await db("tasks").update(updatedtTask).where({id: idToEdit})
+        await db("tasks").update(updatedtTask).where({ id: idToEdit })
         res.status(200).send(`${newTitle} updated successfully`)
+
     } catch (error) {
         console.log(error)
 
@@ -278,7 +362,7 @@ app.put("/tasks/:id", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -288,27 +372,33 @@ app.delete("/tasks/:id", async (req: Request, res: Response) => {
     try {
         const idToDelete = req.params.id
 
-        if(idToDelete !== undefined) {
-            if(typeof idToDelete !== "string" ) {
+        if (idToDelete[0] !== "t") {
+            res.status(400)
+            throw new Error("ID must bagin with 't'")
+        }
+
+        if (idToDelete !== undefined) {
+            if (typeof idToDelete !== "string") {
                 res.status(400)
                 throw new Error("Invalid ID, must be a string")
             }
-            if(idToDelete.length < 1 ) {
+            if (idToDelete.length < 1) {
                 res.status(400)
                 throw new Error("Invalid ID, must have at least 2 characters")
             }
         }
 
-        const [task] = await db("tasks").where({ id: idToDelete })
+        const [task]: TTask[] | undefined[] = await db("tasks").where({ id: idToDelete })
+
         if (task) {
             await db("users_tasks").del().where({ task_id: idToDelete })
             await db("tasks").del().where({ id: idToDelete })
+            res.status(200).send("Task deleted successfully")
         } else {
             res.status(404)
             throw new Error("ID not found, try again.")
         }
 
-        res.status(200).send("Task deleted successfully")
     } catch (error) {
         console.log(error)
 
@@ -319,13 +409,13 @@ app.delete("/tasks/:id", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
 
 
-                        //USERS_TASKS
+//USERS_TASKS
 
 //CreateTaskForUsers
 app.post("/tasks/:taskId/users/:userId", async (req: Request, res: Response) => {
@@ -333,27 +423,38 @@ app.post("/tasks/:taskId/users/:userId", async (req: Request, res: Response) => 
         const userId = req.params.userId
         const taskId = req.params.taskId
 
-        const [ task ] = await db("tasks").where({ id: taskId })
+        if (taskId[0] !== "t") {
+            res.status(400)
+            throw new Error("Invalid TaskId, must begin with 't'")
+        }
+
+        if (userId[0] !== "f") {
+            res.status(400)
+            throw new Error("Invalid TaskId, must begin with 'f'")
+        }
+
+        const [task]: TTask[] | undefined[] = await db("tasks").where({ id: taskId })
 
         if (!task) {
             res.status(404)
-            throw new Error("'taskId' não encontrado")
+            throw new Error("'taskId' not found")
         }
 
-        const [ user ] = await db("users").where({ id: userId })
+        const [user]: TUser[] | undefined[] = await db("users").where({ id: userId })
 
         if (!user) {
             res.status(404)
-            throw new Error("'userId' não encontrado")
+            throw new Error("'userId' not found")
         }
 
         const newTaskForUser = {
-            user_id : userId, 
-            task_id : taskId,
+            user_id: userId,
+            task_id: taskId,
         }
 
-		await db("users_tasks").insert(newTaskForUser)
+        await db("users_tasks").insert(newTaskForUser)
         res.status(200).send(`New task applied successfully`)
+
     } catch (error) {
         console.log(error)
 
@@ -364,7 +465,7 @@ app.post("/tasks/:taskId/users/:userId", async (req: Request, res: Response) => 
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -375,24 +476,36 @@ app.delete("/tasks/:taskId/users/:userId", async (req: Request, res: Response) =
         const userIdToDelete = req.params.userId
         const taskIdToDelete = req.params.taskId
 
-        const [ task ] = await db("tasks").where({ id: taskIdToDelete })
+        if (taskIdToDelete[0] !== "t") {
+            res.status(400)
+            throw new Error("Invalid TaskId, must begin with 't'")
+        }
+
+        if (userIdToDelete[0] !== "f") {
+            res.status(400)
+            throw new Error("Invalid TaskId, must begin with 'f'")
+        }
+
+        const [task]: TTask[] | undefined[] = await db("tasks").where({ id: taskIdToDelete })
 
         if (!task) {
             res.status(404)
-            throw new Error("'taskId' não encontrado")
+            throw new Error("'taskId' not found")
         }
 
-        const [ user ] = await db("users").where({ id: userIdToDelete })
+        const [user]: TUser[] | undefined[] = await db("users").where({ id: userIdToDelete })
 
         if (!user) {
             res.status(404)
-            throw new Error("'userId' não encontrado")
+            throw new Error("'userId' not found")
         }
 
-		await db("users_tasks").del()
-        .where({task_id : taskIdToDelete})
-        .andWhere({user_id : userIdToDelete})
+        await db("users_tasks").del()
+            .where({ task_id: taskIdToDelete })
+            .andWhere({ user_id: userIdToDelete })
+
         res.status(200).send(`Task deleted from user successfully`)
+
     } catch (error) {
         console.log(error)
 
@@ -403,7 +516,7 @@ app.delete("/tasks/:taskId/users/:userId", async (req: Request, res: Response) =
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
         }
     }
 })
@@ -426,20 +539,20 @@ app.get("/tasks/users", async (req: Request, res: Response) => {
         //     .leftJoin("users_tasks", "users_tasks.task_id", "=", "tasks.id")
         //     .leftJoin("users", "users_tasks.user_id", "=", "users.id")
 
-        const tasks = await db("tasks")
+        const tasks: TTask[] = await db("tasks")
 
-        const result = []
+        const result: TTaskWithUsers[] = []
 
         for (let task of tasks) {
             const responsibles = []
-            const users_tasks = await db("users_tasks").where({ task_id: task.id })
+            const users_tasks: TUserTask[] = await db("users_tasks").where({ task_id: task.id })
             
             for (let user_task of users_tasks) {
-                const [ user ] = await db("users").where({ id: user_task.user_id })
+                const [ user ]: TUser[] = await db("users").where({ id: user_task.user_id })
                 responsibles.push(user)
             }
 
-            const newTaskWithUsers = {
+            const newTaskWithUsers: TTaskWithUsers = {
                 ...task,
                 responsibles
             }
@@ -459,7 +572,37 @@ app.get("/tasks/users", async (req: Request, res: Response) => {
         if (error instanceof Error) {
             res.send(error.message)
         } else {
-            res.send("Erro inesperado")
+            res.send("Unexpected Error")
+        }
+    }
+})
+
+//SearchTaskByTitle
+app.get("/tasks/:title", async (req: Request, res: Response) => {
+    try {
+        const q = req.params.title as string | undefined
+
+        if (q === undefined) {
+            res.status(400)
+            throw new Error("Invalid Search, try again")
+        }
+
+        const result = await db("tasks")
+            .where("title", "LIKE", `%${q}%`)
+            .orWhere("description", "LIKE", `%${q}%`)
+
+        res.status(200).send({ task: result })
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Unexpected Error")
         }
     }
 })
